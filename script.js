b9016b5a999fe05a5f02f812a7c8a8ebc0fda07a
pooja036 <khatripooja036@gmail.com>
2025-07-25 08:28:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log('Hello, World!');
+
+console.log(5 + 3 - 2 * 2);
+typeof 5;
+console.log(typeof 5);
+typeof 'Hello, World!';
+console.log(typeof 'Hello, World!');
+ typeof true;
+console.log(typeof true);
+typeof +'anurag';
+console.log(typeof +'anurag');
+
++'100a';
+console.log(+'100a');
+
+'assas' + 100;
+console.log('assas' + 100);
+parseInt('100a');
+console.log(parseInt('100a'));
+
++false;
+console.log(+false);
++true;
+console.log(+true);
+null;
+console.log(null);
+undefined;
+console.log(undefined);
+
++null;
+console.log(+null);
++undefined;
+console.log(+undefined);
+typeof null;
+console.log(typeof null);
+
+let a = 25;
+console.log(a);
+
+a;
+console.log(a);
+
+let b = 20;
+console.log(b);
+
+let userName = 'Anurag';
+let age = 20;
+
+let userIntro = ' Hi I am ' + userName;
+console.log(userIntro);
+
+let userIntro2 = 'Hi I am ' + age + ' years old ';
+console.log(userIntro2);
