643630f5e9dbb8ec3b7231929a78b5ae0bfe7351
pooja036 <khatripooja036@gmail.com>
2025-07-26 09:50:34 +0530

    string methods and properties practice

--- script.js
+++ script.js
@@ -40,6 +40,8 @@ console.log(a);
 a;
 console.log(a);
 
+
+
 let b = 20;
 console.log(b);
 
@@ -51,3 +53,94 @@ console.log(userIntro);
 
 let userIntro2 = 'Hi I am ' + age + ' years old ';
 console.log(userIntro2);
+
+{
+var firstName = 'puja';
+}
+
+let lastName = 'Singh';
+
+let age2 = 20;
+let isHappy = true;
+// firstName = 100;
+
+
+let userIntro3 = 'Hi my name is ' + firstName + ' ' +  lastName + ' and I am ' + age2 + ' years old.' + ' Is it true that I am happy? ' + isHappy;
+console.log(userIntro3);
+
+const hoursInDay = 24;
+console.log(hoursInDay);
+
+// alert('This is an alert box!');
+// confirm('Do you want to proceed?');
+
+// const isConfirmed = confirm('Do you want to proceed?');
+// prompt('Please enter your name');
+
+// const userInputName = prompt('Please enter your name');
+// console.log(userInputName);
+
+
+const message = 'Hello, World!';
+console.log(message);
+
+const capitalMessage = message.toUpperCase();
+console.log(capitalMessage);
+
+const lowerMessage = message.toLowerCase();
+console.log(lowerMessage);
+
+const faltuMessage = '    hello, i am puja';
+const trimmedMessage = faltuMessage.trim().toLowerCase();
+console.log(trimmedMessage);
+
+// const finalMessage = message.includes('i am  puja');
+// console.log(finalMessage); 
+
+// const finalMessage = message.indexOf('World');
+// console.log(finalMessage);
+
+const finalMessage = message.replace('World', 'Anurag');
+console.log(finalMessage);
+
+const finalMessage2 = capitalMessage.concat(', ' + finalMessage);
+console.log(finalMessage2);
+
+const lastFourDigits = '7891';
+const cardNumber = lastFourDigits.padEnd(16, '*');
+console.log(cardNumber);
+
+const finalmeassage = message.split(1);
+console.log(finalmeassage);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
